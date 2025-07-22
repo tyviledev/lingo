de449f19cda3f4c44c89fe20f9089509f95be0f1
Ty <typhn.sa@gmail.com>
2025-07-22 14:59:02 +0930

    create landing page layout

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p className="text-green-500">Hello Lingo</p>
-      <Button className="bg-destructive">Click Me</Button>
+      <p>Hello Lingo</p>
     </div>
   );
 }
