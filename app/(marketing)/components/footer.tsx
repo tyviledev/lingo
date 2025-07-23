e6757ee7e6d5858d5daae90bf836e3438aa8b118
Ty <typhn.sa@gmail.com>
2025-07-23 23:13:51 +0930

    footer design

--- app/(marketing)/components/footer.tsx
+++ app/(marketing)/components/footer.tsx
@@ -1,7 +1,73 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
   return (
-    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 px-4">
-      Footer
+    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button
+          size="lg"
+          variant="ghost"
+          className="flex items-center justify-center w-fit"
+        >
+          <Image src="/flag_thai.svg" height={30} width={30} alt="Thai Flag" />
+        </Button>
+        <Button
+          size="lg"
+          variant="ghost"
+          className="flex items-center justify-center w-fit"
+        >
+          <Image
+            src="/flag_indonesia.svg"
+            height={30}
+            width={30}
+            alt="Indonesian Flag"
+          />
+        </Button>
+        <Button
+          size="lg"
+          variant="ghost"
+          className="flex items-center justify-center w-fit"
+        >
+          <Image src="/flag_laos.svg" height={30} width={30} alt="Laos Flag" />
+        </Button>
+        <Button
+          size="lg"
+          variant="ghost"
+          className="flex items-center justify-center w-fit"
+        >
+          <Image
+            src="/flag_malaysia.svg"
+            height={30}
+            width={30}
+            alt="Malaysian Flag"
+          />
+        </Button>
+        <Button
+          size="lg"
+          variant="ghost"
+          className="flex items-center justify-center w-fit"
+        >
+          <Image
+            src="/flag_phillipines.svg"
+            height={30}
+            width={30}
+            alt="Phillipine Flag"
+          />
+        </Button>
+        <Button
+          size="lg"
+          variant="ghost"
+          className="flex items-center justify-center w-fit"
+        >
+          <Image
+            src="/flag_vietnam.svg"
+            height={30}
+            width={30}
+            alt="Vietnam Flag"
+          />
+        </Button>
+      </div>
     </footer>
   );
 };
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,55 @@
 import { Button } from "@/components/ui/button";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  SignUpButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <p>Hello Lingo</p>
+    <div className="max-w-[998px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[600px] mb-8 lg:mb-0">
+        <Image src="/hero.svg" fill alt="hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-3">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 text-center">
+          Learn, practice, and master new languages with Lingo.
+        </h1>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedOut>
+            <SignUpButton mode="modal">
+              <Button size="lg" variant="secondary">
+                Get Started
+              </Button>
+            </SignUpButton>
+            <SignInButton mode="modal">
+              <Button size="lg" variant="primaryOutline">
+                I already have an account
+              </Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <Button
+              size="lg"
+              variant="secondary"
+              className="w-full max-w-2/3"
+              asChild
+            >
+              <Link href="/learn">Continue Learning</Link>
+            </Button>
+          </SignedIn>
+        </ClerkLoaded>
+      </div>
     </div>
   );
 }
