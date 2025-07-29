8624e04f74cc0b4f23d890db8b15458e377df664
tyviledev <ty.jvile.developer@gmail.com>
2025-07-29 23:08:33 +0930

    add user progress to schema and add relations to courses

--- db/schema/courses.ts
+++ db/schema/courses.ts
@@ -1,3 +1,5 @@
+import { userProgress } from "@/db/schema/user-progress";
+import { relations } from "drizzle-orm";
 import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
@@ -5,3 +7,7 @@ export const courses = pgTable("courses", {
   title: text("title").notNull(),
   imageSrc: text("image_src").notNull(),
 });
+
+export const courseRelations = relations(courses, ({ many }) => ({
+  userProgress: many(userProgress),
+}));
---
+++ db/schema/user-progress.ts
@@ -0,0 +1,21 @@
+import { courses } from "@/db/schema/courses";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, text } from "drizzle-orm/pg-core";
+
+export const userProgress = pgTable("user_progress", {
+  userId: text("user_id").primaryKey(),
+  userName: text("user_name").notNull().default("User"),
+  userImageSrc: text("user_image_src").notNull().default("/user_avatar.svg"),
+  activeCourseId: integer("active_course_id").references(() => courses.id, {
+    onDelete: "cascade",
+  }),
+  hearts: integer("hearts").notNull().default(5),
+  points: integer("points").notNull().default(5),
+});
+
+export const userProgressRelations = relations(userProgress, ({ one }) => ({
+  activeCourse: one(courses, {
+    fields: [userProgress.activeCourseId],
+    references: [courses.id],
+  }),
+}));
