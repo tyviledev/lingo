4661b9d50b56b14b5886c5375b403319102ec293
tyviledev <ty.jvile.developer@gmail.com>
2025-08-20 00:20:30 +0930

    create getunits method in queries.ts

--- db/index.ts
+++ db/index.ts
@@ -3,11 +3,24 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import * as coursesSchema from "@/db/schema/courses";
 import * as userProgressSchema from "@/db/schema/user-progress";
+import * as challengeOptionsSchema from "@/db/schema/challenge-options";
+import * as challengeProgressSchema from "@/db/schema/challenge-progress";
+import * as challengesSchema from "@/db/schema/challenges";
+import * as unitsSchema from "@/db/schema/units";
+import * as lessonsSchema from "@/db/schema/lessons";
 
 const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle({
   client: sql,
-  schema: { ...coursesSchema, ...userProgressSchema },
+  schema: {
+    ...coursesSchema,
+    ...userProgressSchema,
+    ...challengeOptionsSchema,
+    ...challengeProgressSchema,
+    ...challengesSchema,
+    ...unitsSchema,
+    ...lessonsSchema,
+  },
 });
 
 export default db;
--- db/queries.ts
+++ db/queries.ts
@@ -11,6 +11,10 @@ import { auth } from "@clerk/nextjs/server";
 // schema
 import { userProgress } from "@/db/schema/user-progress";
 import { courses } from "@/db/schema/courses";
+import { units } from "@/db/schema/units";
+import { challenges } from "@/db/schema/challenges";
+import { challengeProgress } from "@/db/schema/challenge-progress";
+import { lessons } from "@/db/schema/lessons";
 
 // get user progress
 
@@ -30,6 +34,45 @@ export const getUserProgress = cache(async () => {
   });
 
   return data;
+});
+
+export const getUnits = cache(async () => {
+  const userProgress = await getUserProgress();
+
+  if (!userProgress?.activeCourseId) {
+    return [];
+  }
+
+  const data = await db.query.units.findMany({
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        with: {
+          challenges: {
+            with: {
+              challengeProgress: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const normalizedData = data.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      const allCompletedChallenges = lesson.challenges.every((challenge) => {
+        return (
+          challenge.challengeProgress &&
+          challenge.challengeProgress.length > 0 &&
+          challenge.challengeProgress.every((progress) => progress.completed)
+        );
+      });
+      return { ...lessons, completed: allCompletedChallenges };
+    });
+    return { ...unit, lessons: lessonsWithCompletedStatus };
+  });
+
+  return normalizedData;
 });
 
 // get all courses
